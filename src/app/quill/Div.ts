32992a9b5a4de6f026f6fb260c3e9ad8ca2eee1e
Birendra <birendra@rudrainnovative.in>
2024-03-28 15:30:35 +0530

    Bug fix

--- src/app/quill/Div.ts
+++ src/app/quill/Div.ts
@@ -3,7 +3,7 @@ import Quill from 'quill';
 const Block:any = Quill.import('blots/block');
 
 export default class Div extends Block {
-  static blotName = 'div';
+  static blotName = 'div1'; 
   static tagName = 'div';
 
   static create(value: any) {
